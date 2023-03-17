2e120b5dbbf8941974930e06c94e5ef713591fc2
GabrielGil-Castro <gsgc05@gmail.com>
2023-03-17 16:30:27 -0300

    requisito 3 completo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </div>
   );
