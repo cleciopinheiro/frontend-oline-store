b5b96169d8e72da170dcc911072339dac31f4457
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-03-17 18:06:45 -0300

    Feat: Requisito 4

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { getCategories } from '../services/api';
+import './SideBar.css';
+import { Link } from 'react-router-dom';
 
 export default class SideBar extends Component {
 
-  render() {
-    const categories = getCategories();
-    console.log();
+  state = {
+    categories: [],
+  }
 
+  async componentDidMount() {
+    const data = await getCategories();
+    this.setState({
+      categories: data,
+    })
+  }
+
+  render() {
+    const { categories } = this.state
     return (
-      <div>
-        {
-          categories.map((categorie) => {
-            <input
-              type="radio"
-              
-            />
-          })
-        }
-      </div>
+      <form className="side-bar">
+        <h2>Categorias</h2>
+      { categories.map((categorie) => (
+        <label 
+          htmlFor={ categorie.id }
+          key={ categorie.id }
+          data-testid="category"
+        >
+          <input
+          id={ categorie.id }
+          name='categories'
+          type="radio" />
+          { categorie.name }
+        </label>
+
+      )) }
+              <Link to="/shopping-cart">
+          <button
+            type="button"
+            data-testid="shopping-cart-button"
+          >
+            Ir para o carrinho
+          </button>
+        </Link>
+      </form>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,15 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import SideBar from '../components/SideBar'; 
+import SideBar from '../components/SideBar';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-        <Link to="/shopping-cart">
-          <button
-            type="button"
-            data-testid="shopping-cart-button"
-          >
-            Carrinho
-
-          </button>
-        </Link>
-        <p>Categorias</p>
+      <div className="container" data-testid="home-initial-message">
         <SideBar />
+        <div className="main-page">
+          <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        </div>
       </div>
     );
   }
